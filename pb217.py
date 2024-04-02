3bbd9253f4ae6e70206e7402971db011f1d2d4c1
NirmouMounir <mounir.abakhour@gmail.com>
2024-04-02 11:30:56 +0200

    done

---
+++ pb217.py
@@ -0,0 +1,12 @@
+def contains_duplicate(numlist) -> bool:
+    return len(set(numlist)) < len(numlist)
+
+
+nums1 = [1, 2, 3, 1]
+nums2 = [1, 2, 3, 4]
+nums3 = [1, 1, 1, 3, 3, 4, 3, 2, 4, 2]
+
+
+print(contains_duplicate(nums1))
+print(contains_duplicate(nums2))
+print(contains_duplicate(nums3))
